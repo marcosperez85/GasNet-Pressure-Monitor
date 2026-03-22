e1720513e9497c6eb19c265736e844e0a2508418
Marcos Perez <perezmarcos04@yahoo.com.ar>
2026-03-22 18:13:41 -0300

    fix: elimino referencia a API Gateway que fue removido del manifest

--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ let rootElement = EMBED.getRootElement();
 let data = EMBED.getComponent().schema.data;
 
 export const $GOOGLE_MAPS_API_KEY = data.GOOGLE_MAPS_API_KEY.manual;
-export const $API_GATEWAY_URL = data.API_GATEWAY_URL.manual;
-export const $API_GATEWAY_API_KEY = data.API_GATEWAY_API_KEY.manual;
 
 $(document).ready(async function() {
         
