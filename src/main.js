ea7f39458b466044c1a697c5e6e5bda89dcab9be
Marcos Perez <perezmarcos04@yahoo.com.ar>
2026-03-22 20:06:42 -0300

    feat: agrego variables del manifest y las exporto para poder usarlas en el chart.js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,26 @@ import { loadMap } from '../scripts/map.js';
 let rootElement = EMBED.getRootElement();
 let data = EMBED.getComponent().schema.data;
 
+// Variables traidas del manifest
 export const $GOOGLE_MAPS_API_KEY = data.GOOGLE_MAPS_API_KEY.manual;
+export const inputPEntradaHist = data.inputPEntradaHist;
+export const inputCaudalHist = data.inputCaudalHist;
+export const startDateGlobal = data.startDateGlobal;
+export const endDateGlobal = data.endDateGlobal;
 
-$(document).ready(async function() {
-        
+$(document).ready(async function () {
+
+    // Exportar variables al objeto global para evitar importaciones circulares
+    window.GasNetConfig = {
+        inputPEntradaHist,
+        inputCaudalHist,
+        startDateGlobal,
+        endDateGlobal
+    };
+
     setupDistributionZones();
     setupNavigation();
     initChart();
     loadMap($GOOGLE_MAPS_API_KEY);
-    
+
 });
