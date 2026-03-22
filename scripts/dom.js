a57d18d131ae2f72770b4a468cd051c5f7a627f2
Marcos Perez <perezmarcos04@yahoo.com.ar>
2026-03-22 14:23:27 -0300

    Refactor: convierto a jQuery

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,6 +1,6 @@
 const DOM = {
-    segmentList: document.getElementById('segmentList'),
-    noSelection: document.getElementById('noSelectionMessage'),
-    chart: document.getElementById('trendChart'),
-    map: document.getElementById('map')
+    segmentList: $('#segmentList')[0],
+    noSelection: $('#noSelectionMessage')[0],
+    chart: $('#trendChart')[0],
+    map: $('#map')[0]
 };
