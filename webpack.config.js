6b810695f09366a11af6a6c968baa74c6178e6f2
Marcos Perez <perezmarcos04@yahoo.com.ar>
2026-03-27 19:58:47 -0300

    fix: corrijo rutas para que se copie el spec y el spec.zip en el empaquetado final

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,18 @@ const pluginName = "GasNet-Pressure-Monitor";
 // 👉 OPCIONAL: ruta directa a Operations Hub
 const endAppPath = "C:/Program Files/Proficy/Operations Hub/iqp-endapp";
 
+// Ruta de salida para el build
+const outputPath = path.resolve(
+  __dirname,
+  endAppPath + "/public/custom/default/" + pluginName
+);
+
 module.exports = {
   mode: 'production',
   entry: './src/main.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(
-      __dirname,
-      endAppPath + "/public/custom/default/" + pluginName
-    ),
+    path: outputPath,
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -29,18 +32,27 @@ module.exports = {
         { from: 'src/manifest.json', to: '.' },
         { from: 'src/preview.png', to: '.' },
         { from: 'src/customIcon.png', to: '.' },
-
-        {
-          from: './scripts/**/*',
-          to: './'
-        }
+        { from: 'scripts/**/*', to: './' },
+        { from: 'spec/*spec.js', to: './' }
       ]
     }),
 
+    // ZIP del plugin
     new ZipPlugin({
       path: '../zip',
       pathPrefix: pluginName,
       filename: pluginName + '.zip'
+    }),
+
+    // ZIP del spec
+    new ZipPlugin({
+      filename: 'spec.zip',
+      path: outputPath,
+      include: [/.*spec\.js$/],
+      pathMapper: function (assetPath) {
+        // Elimina cualquier ruta de directorio para que los archivos queden en la raíz del ZIP
+        return assetPath.split('/').pop();
+      }
     })
   ],
 
