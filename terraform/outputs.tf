0f9b5e0382417e020c5b24b1804eae0f3440d3ad
Marcos Perez <marcospereztech@proton.me>
2026-09-23 17:25:55 -0300

    feat: agrego como output el bucket donde se deplegó el frontend y la URL de CloudFront

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,15 @@ output "api_key" {
   value     = aws_api_gateway_api_key.api_key.value
   sensitive = true
 }
+
+output "frontend_url" {
+  value = "https://${aws_cloudfront_distribution.frontend.domain_name}"
+}
+
+output "frontend_bucket" {
+  value = aws_s3_bucket.frontend.id
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
